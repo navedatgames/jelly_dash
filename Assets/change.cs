d524d57c7f1b0e5f3af97ef25339f037a04671d9
Naved <navedatgames@gmail.com>
2020-09-16 22:05:31 +0530

    ui elements

--- Assets/change.cs
+++ Assets/change.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class change : MonoBehaviour
 {
-	public Material yellow;
 	public Material black;
 	public Material skin;
 	public Material grey;
 
     void Start()
     {
-		transform.FindChild("Body").GetComponent<Renderer>().material = yellow;
-		transform.FindChild("head").GetComponent<Renderer>().material = skin;
-		transform.FindChild("Left_Am").GetComponent<Renderer>().material = skin;
-		transform.FindChild("Left_foot").GetComponent<Renderer>().material = grey;
-		transform.FindChild("Left_Hand_").GetComponent<Renderer>().material = skin;
-		transform.FindChild("Left_leg").GetComponent<Renderer>().material =black;
-		transform.FindChild("Right_arm").GetComponent<Renderer>().material =skin;
-		transform.FindChild("Right_foot").GetComponent<Renderer>().material = grey;
-		transform.FindChild("Right_hand").GetComponent<Renderer>().material = skin;
-		transform.FindChild("Right_leg").GetComponent<Renderer>().material = black;
+		transform.FindChild("polySurface1").GetComponent<Renderer>().material = grey;
+		transform.FindChild("polySurface2").GetComponent<Renderer>().material = black;
+		transform.FindChild("polySurface3").GetComponent<Renderer>().material = skin;
+
     }
 
     
---
+++ Assets/change1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change1 : MonoBehaviour
+{
+	public Material black;
+	public Material yellow;
+	public Material grey;
+	public Material skin;
+
+	void Start()
+	{
+		transform.FindChild("Body").GetComponent<Renderer>().material = yellow;
+		transform.FindChild("head").GetComponent<Renderer>().material = skin;
+		transform.FindChild("Left_Am").GetComponent<Renderer>().material = skin;
+		transform.FindChild("Left_Hand_").GetComponent<Renderer>().material = skin;
+		transform.FindChild("Right_arm").GetComponent<Renderer>().material = skin;
+		transform.FindChild("Right_hand").GetComponent<Renderer>().material = skin;
+		transform.FindChild("Left_leg").GetComponent<Renderer>().material = black;
+		transform.FindChild("Right_leg").GetComponent<Renderer>().material = black;
+		transform.FindChild("Left_foot").GetComponent<Renderer>().material = grey;
+		transform.FindChild("Right_foot").GetComponent<Renderer>().material = grey;
+
+	}
+
+
+}
